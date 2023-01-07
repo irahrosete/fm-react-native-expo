5b84219259fa53a289497f450515e77f9eb18206
irah rosete <irah.rosete@outlook.com>
2023-01-07 13:23:13 +1100

    add and style text input

--- screens/ColorPaletteModal.js
+++ screens/ColorPaletteModal.js
@@ -1,12 +1,28 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, TextInput, StyleSheet } from 'react-native'
 
 const ColorPaletteModal = () => {
+  const [name, setName] = useState('')
   return (
-    <View>
+    <View style={styles.container}>
+      <TextInput style={styles.input} value={name} onChangeText={setName} />
+
       <Text>This is the colour palette modal</Text>
     </View>
   )
 }
 
+const styles = StyleSheet.create({
+  input: {
+    borderColor: 'grey',
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
+  },
+  container: {
+    padding: 5,
+    backgroundColor: 'white',
+    flex: 1,
+  },
+})
 export default ColorPaletteModal
