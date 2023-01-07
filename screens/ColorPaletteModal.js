d2b0c526a40baf0ac59439716da212fd2283c374
irah rosete <irah.rosete@outlook.com>
2023-01-07 13:30:47 +1100

    add and style submit button

--- screens/ColorPaletteModal.js
+++ screens/ColorPaletteModal.js
@@ -1,13 +1,21 @@
 import React, { useState } from 'react'
 import { View, Text, TextInput, StyleSheet } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 const ColorPaletteModal = () => {
   const [name, setName] = useState('')
   return (
     <View style={styles.container}>
-      <TextInput style={styles.input} value={name} onChangeText={setName} />
-
-      <Text>This is the colour palette modal</Text>
+      <Text style={styles.name}>Name of the palette</Text>
+      <TextInput
+        style={styles.input}
+        value={name}
+        onChangeText={setName}
+        placeholder="Palette name"
+      />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Submit</Text>
+      </TouchableOpacity>
     </View>
   )
 }
@@ -18,11 +26,26 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
     borderRadius: 5,
+    marginBottom: 50,
   },
   container: {
-    padding: 5,
+    padding: 10,
     backgroundColor: 'white',
     flex: 1,
+  },
+  button: {
+    height: 40,
+    backgroundColor: 'teal',
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  name: {
+    marginBottom: 10,
   },
 })
 export default ColorPaletteModal
